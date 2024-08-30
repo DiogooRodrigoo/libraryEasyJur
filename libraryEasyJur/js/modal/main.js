97c6c2a7bdaa52b8eddf2e9a433f00819c1741e5
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-08-30 14:04:10 -0300

    update remote repository

--- libraryEasyJur/js/modal/main.js
+++ libraryEasyJur/js/modal/main.js
@@ -1,15 +1,14 @@
+const modal = document.getElementById("bookModal");
+const closeModal = document.getElementsByClassName("close")[0];
+
 function openModal(imgSrc, name, description, author) {
   document.getElementById("modalImg").src = imgSrc;
   document.getElementById("modalName").innerText = `Livro: ${name}`;
   document.getElementById("modalDescription").innerText = description;
   document.getElementById("modalAuthor").innerText = `Autor: ${author}`;
 
-  const modal = document.getElementById("bookModal");
   modal.style.display = "block";
 }
-
-const modal = document.getElementById("bookModal");
-const closeModal = document.getElementsByClassName("close")[0];
 
 closeModal.onclick = function () {
   modal.style.display = "none";
